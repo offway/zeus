9ebf089c4312142538e4d0d965e538a99f6f44be
weinan <weinan@offway-art.com>
2019-04-19 12:02:35 +0800

    优化订单

--- src/main/java/cn/offway/zeus/controller/OrderController.java
+++ src/main/java/cn/offway/zeus/controller/OrderController.java
@@ -40,6 +40,7 @@ import cn.offway.zeus.service.PhPreorderInfoService;
 import cn.offway.zeus.utils.CommonResultCode;
 import cn.offway.zeus.utils.JsonResult;
 import cn.offway.zeus.utils.JsonResultHelper;
+import cn.offway.zeus.utils.MathUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -188,13 +189,21 @@ public class OrderController {
 		return jsonResultHelper.buildSuccessJsonResult(page3);
 	}
 	
-	@SuppressWarnings("unchecked")
+	/*@SuppressWarnings("unchecked")
 	@ApiOperation("待付款订单详情")
 	@GetMapping("/info/pending")
 	public JsonResult infoPending(@ApiParam("预订单号") @RequestParam String preOrderNo){
 		
 		PhPreorderInfo phPreorderInfo = phPreorderInfoService.findByOrderNoAndStatus(preOrderNo, "0");
-		Map<String, Object> resultMap = JSON.parseObject(JSON.toJSONString(phPreorderInfo), Map.class);
+		Map<String, Object> resultMap = new HashMap<>();//JSON.parseObject(JSON.toJSONString(phPreorderInfo), Map.class);
+		
+		resultMap.put("amount", phPreorderInfo.getAmount());
+		resultMap.put("orderNo", phPreorderInfo.getOrderNo());
+		resultMap.put("mailFee", phPreorderInfo.getMailFee());
+		resultMap.put("price", phPreorderInfo.getPrice());
+		resultMap.put("status", phPreorderInfo.getStatus());
+		resultMap.put("voucherAmount", phPreorderInfo.getVoucherAmount());
+		resultMap.put("walletAmount", phPreorderInfo.getWalletAmount());
 		
 		
 		PhAddress phAddress = phAddressService.findOne(phPreorderInfo.getAddrId());
@@ -219,40 +228,86 @@ public class OrderController {
 		
 		return jsonResultHelper.buildSuccessJsonResult(resultMap);
 
-	}
+	}*/
 	
-	@SuppressWarnings("unchecked")
 	@ApiOperation("订单详情")
 	@GetMapping("/info")
-	public JsonResult info(@ApiParam("订单号") @RequestParam String orderNo){
+	public JsonResult info(
+			@ApiParam("预订单号[仅支持待付款状态下查询]") @RequestParam(required = false) String preOrderNo,
+			@ApiParam("订单号") @RequestParam(required = false) String orderNo){
 
-		PhOrderInfo phOrderInfo = phOrderInfoService.findByOrderNo(orderNo);
 		
-		Map<String, Object> resultMap = JSON.parseObject(JSON.toJSONString(phOrderInfo), Map.class);
+		Double amount = 0D;
+		Double mailFee = 0D;
+		Double price = 0D;
+		String status = null;
+		Double voucherAmount = 0D;
+		Double walletAmount = 0D;
+		Long addrId = null;
+		List<PhOrderGoods> goods = new ArrayList<>();
+		Map<String, Object> addrMap = new HashMap<>();
+
+
+		
+		Map<String, Object> resultMap = new HashMap<>();
+
+		if(StringUtils.isNotBlank(preOrderNo)){
+			PhPreorderInfo phPreorderInfo = phPreorderInfoService.findByOrderNoAndStatus(preOrderNo, "0");
+			amount = phPreorderInfo.getAmount();
+			orderNo = phPreorderInfo.getOrderNo();
+			mailFee = phPreorderInfo.getMailFee();
+			price = phPreorderInfo.getPrice();
+			status = "0";
+			voucherAmount = phPreorderInfo.getVoucherAmount();
+			walletAmount = phPreorderInfo.getWalletAmount();
+			List<PhOrderInfo> orderInfos = phOrderInfoService.findByPreorderNoAndStatus(phPreorderInfo.getOrderNo(), "0");
+			for (PhOrderInfo phOrderInfo : orderInfos) {
+				goods.addAll(phOrderGoodsService.findByOrderNo(phOrderInfo.getOrderNo()));
+			}
+			
+		}else{
+			PhOrderInfo phOrderInfo = phOrderInfoService.findByOrderNo(orderNo);
+			amount = phOrderInfo.getAmount();
+			orderNo = phOrderInfo.getOrderNo();
+			mailFee = phOrderInfo.getMailFee();
+			price = phOrderInfo.getPrice();
+			status = phOrderInfo.getStatus();
+			voucherAmount = MathUtils.add(phOrderInfo.getMVoucherAmount(), phOrderInfo.getPVoucherAmount());
+			walletAmount = phOrderInfo.getWalletAmount();
+			addrId = phOrderInfo.getAddrId();
+			goods = phOrderGoodsService.findByOrderNo(orderNo);
+
+			
+		}
+		
+		resultMap.put("goods", goods);
+		resultMap.put("amount", amount);
+		resultMap.put("orderNo", orderNo);
+		resultMap.put("mailFee", mailFee);
+		resultMap.put("price", price);
+		resultMap.put("status", status);
+		resultMap.put("voucherAmount", voucherAmount);
+		resultMap.put("walletAmount", walletAmount);
 		
 		//0-已下单,1-已付款,2-已发货,3-已收货,4-取消
-		String status = phOrderInfo.getStatus();
 		if("0".equals(status) || "1".equals(status)|| "4".equals(status)){
-			PhAddress phAddress = phAddressService.findOne(phOrderInfo.getAddrId());
-			Map<String, Object> addrMap = new HashMap<>();
+			PhAddress phAddress = phAddressService.findOne(addrId);
 			addrMap.put("city", phAddress.getCity());
 			addrMap.put("content", phAddress.getContent());
 			addrMap.put("county", phAddress.getCounty());
 			addrMap.put("phone", phAddress.getPhone());
 			addrMap.put("province", phAddress.getProvince());
 			addrMap.put("realName", phAddress.getRealName());
-			resultMap.put("address", addrMap);
 		}else{
 			
 			PhOrderExpressInfo phOrderExpressInfo = phOrderExpressInfoService.findByOrderNoAndType(orderNo, "0");
-			Map<String, Object> addrMap = new HashMap<>();
+			
 			addrMap.put("city", phOrderExpressInfo.getToCity());
 			addrMap.put("content", phOrderExpressInfo.getToContent());
 			addrMap.put("county", phOrderExpressInfo.getToCounty());
 			addrMap.put("phone", phOrderExpressInfo.getToPhone());
 			addrMap.put("province", phOrderExpressInfo.getToProvince());
 			addrMap.put("realName", phOrderExpressInfo.getToRealName());
-			resultMap.put("address", addrMap);
 			
 			if(StringUtils.isNotBlank(phOrderExpressInfo.getMailNo())){
 				//kuaidi100Service.query(phOrderInfo.getExpressCode(), phOrderInfo.getMailNo(), phAddress.getPhone())
@@ -262,8 +317,9 @@ public class OrderController {
 			}
 		}
 		
-		List<PhOrderGoods> goods = phOrderGoodsService.findByOrderNo(orderNo);
-		resultMap.put("goods", goods);
+		resultMap.put("address", addrMap);
+
+		
 		
 		return jsonResultHelper.buildSuccessJsonResult(resultMap);
 
