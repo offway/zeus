42ebc03cf4a6cbdfb7f9df596cdc9b4063637b4f
XiaZhengXin <xzx@xiazhengxin.name>
2019-10-22 15:56:27 +0800

    奖品兑换 接口加入写锁。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/main/java/cn/offway/zeus/controller/activity/ActivityController.java
+++ src/main/java/cn/offway/zeus/controller/activity/ActivityController.java
@@ -1,14 +1,8 @@
 package cn.offway.zeus.controller.activity;
 
-import cn.offway.zeus.service.PhGoodsStockService;
-import com.qiniu.util.Json;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.*;
-
 import cn.offway.zeus.domain.PhLaborPrize;
 import cn.offway.zeus.repository.PhLaborPrizeRepository;
+import cn.offway.zeus.service.PhGoodsStockService;
 import cn.offway.zeus.service.PhVoucherInfoService;
 import cn.offway.zeus.utils.CommonResultCode;
 import cn.offway.zeus.utils.JsonResult;
@@ -16,95 +10,97 @@ import cn.offway.zeus.utils.JsonResultHelper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-@Api(tags={"活动"})
+@Api(tags = {"活动"})
 @RestController
 @RequestMapping("/activity")
 public class ActivityController {
 
-	@Autowired
-	private JsonResultHelper jsonResultHelper;
-	
-	@Autowired
-	private PhLaborPrizeRepository phLaborPrizeRepository;
-	
-	@Autowired
-	private PhVoucherInfoService phVoucherInfoService;
+    @Autowired
+    private JsonResultHelper jsonResultHelper;
 
-	@Autowired
-	private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private PhLaborPrizeRepository phLaborPrizeRepository;
 
-	@Autowired
-	private PhGoodsStockService phGoodsStockService;
-	
-	@ApiOperation(value = "奖品兑换")
-	@PostMapping("/exchange")
-	public JsonResult exchange(
-			@ApiParam("用户ID") @RequestParam Long userId,
-			@ApiParam("抽奖码") @RequestParam String code,
-			@ApiParam("活动批次") @RequestParam String type){
-		
-		String name = null;
-		PhLaborPrize phLaborPrize = phLaborPrizeRepository.findByTypeAndStatusAndRemark(type, "0", code);
-		if(null!=phLaborPrize){
-			phLaborPrize.setStatus("1");
-			phLaborPrize.setUserId(userId);
-			phLaborPrizeRepository.save(phLaborPrize);
-			name = phLaborPrize.getName();
-			Long vpid = phLaborPrize.getVoucherProjectId();
-			if(null!=vpid){
-				boolean result = phVoucherInfoService.giveVoucher(userId, vpid);
-				if(result){
-					return jsonResultHelper.buildSuccessJsonResult(name);
-				}else{
-					return jsonResultHelper.buildFailJsonResult(CommonResultCode.SYSTEM_ERROR);
-				}
-			}
-			return jsonResultHelper.buildSuccessJsonResult(name);
+    @Autowired
+    private PhVoucherInfoService phVoucherInfoService;
 
-		}else{
-			return jsonResultHelper.buildFailJsonResult(CommonResultCode.CODE_ERROR);
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
-		}
-		
-	}
-	
-	@ApiOperation(value = "潮流展抽奖")
-	@PostMapping("/lottery/exhibition")
-	public JsonResult exhibition(){
-		
-		String name = null;
-		PhLaborPrize phLaborPrize = phLaborPrizeRepository.lottery("3");
-		if(null!=phLaborPrize){
-			phLaborPrize.setStatus("1");
-			phLaborPrizeRepository.save(phLaborPrize);
-			name = phLaborPrize.getName();
-		}
-		
-		return jsonResultHelper.buildSuccessJsonResult(name);
-	}
+    @Autowired
+    private PhGoodsStockService phGoodsStockService;
 
-	@ApiOperation(value = "chillhigh分享")
-	@PostMapping("/chillhigh")
-	public JsonResult chillhigh(@ApiParam("用户ID") @RequestParam Long userId){
-		stringRedisTemplate.opsForValue().set("zeus.chillhigh.share."+userId,"0",10, TimeUnit.DAYS);
-		return jsonResultHelper.buildSuccessJsonResult(null);
-	}
+    @ApiOperation(value = "奖品兑换")
+    @PostMapping("/exchange")
+    @Transactional
+    public JsonResult exchange(
+            @ApiParam("用户ID") @RequestParam Long userId,
+            @ApiParam("抽奖码") @RequestParam String code,
+            @ApiParam("活动批次") @RequestParam String type) {
+        String name = null;
+        PhLaborPrize phLaborPrize = phLaborPrizeRepository.findByTypeAndStatusAndRemark(type, "0", code);
+        if (null != phLaborPrize) {
+            phLaborPrize.setStatus("1");
+            phLaborPrize.setUserId(userId);
+            phLaborPrizeRepository.save(phLaborPrize);
+            name = phLaborPrize.getName();
+            Long vpid = phLaborPrize.getVoucherProjectId();
+            if (null != vpid) {
+                boolean result = phVoucherInfoService.giveVoucher(userId, vpid);
+                if (result) {
+                    return jsonResultHelper.buildSuccessJsonResult(name);
+                } else {
+                    return jsonResultHelper.buildFailJsonResult(CommonResultCode.SYSTEM_ERROR);
+                }
+            }
+            return jsonResultHelper.buildSuccessJsonResult(name);
+        } else {
+            return jsonResultHelper.buildFailJsonResult(CommonResultCode.CODE_ERROR);
+        }
+    }
 
-	@ApiOperation(value = "chillhigh初始化")
-	@GetMapping("/chillhigh/init")
-	public JsonResult chillhighinit(@ApiParam("用户ID") @RequestParam Long userId){
-		Map<String,Object> map = new HashMap<>();
-		String isShare = stringRedisTemplate.opsForValue().get("zeus.chillhigh.share."+userId);
-		map.put("isShare", StringUtils.isNotBlank(isShare));
-		map.put("now", new Date());
-		int stock = phGoodsStockService.sumStock(4465L);
-		map.put("isSellOut", stock <= 0);
-		return jsonResultHelper.buildSuccessJsonResult(map);
-	}
+    @ApiOperation(value = "潮流展抽奖")
+    @PostMapping("/lottery/exhibition")
+    public JsonResult exhibition() {
+
+        String name = null;
+        PhLaborPrize phLaborPrize = phLaborPrizeRepository.lottery("3");
+        if (null != phLaborPrize) {
+            phLaborPrize.setStatus("1");
+            phLaborPrizeRepository.save(phLaborPrize);
+            name = phLaborPrize.getName();
+        }
+
+        return jsonResultHelper.buildSuccessJsonResult(name);
+    }
+
+    @ApiOperation(value = "chillhigh分享")
+    @PostMapping("/chillhigh")
+    public JsonResult chillhigh(@ApiParam("用户ID") @RequestParam Long userId) {
+        stringRedisTemplate.opsForValue().set("zeus.chillhigh.share." + userId, "0", 10, TimeUnit.DAYS);
+        return jsonResultHelper.buildSuccessJsonResult(null);
+    }
+
+    @ApiOperation(value = "chillhigh初始化")
+    @GetMapping("/chillhigh/init")
+    public JsonResult chillhighinit(@ApiParam("用户ID") @RequestParam Long userId) {
+        Map<String, Object> map = new HashMap<>();
+        String isShare = stringRedisTemplate.opsForValue().get("zeus.chillhigh.share." + userId);
+        map.put("isShare", StringUtils.isNotBlank(isShare));
+        map.put("now", new Date());
+        int stock = phGoodsStockService.sumStock(4465L);
+        map.put("isSellOut", stock <= 0);
+        return jsonResultHelper.buildSuccessJsonResult(map);
+    }
 }
--- src/main/java/cn/offway/zeus/repository/PhLaborPrizeRepository.java
+++ src/main/java/cn/offway/zeus/repository/PhLaborPrizeRepository.java
@@ -1,12 +1,12 @@
 package cn.offway.zeus.repository;
 
+import cn.offway.zeus.domain.PhLaborPrize;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
-import cn.offway.zeus.domain.PhLaborPrize;
-import java.lang.String;
-import java.util.List;
+import javax.persistence.LockModeType;
 
 /**
  * 劳动节活动奖品Repository接口
@@ -14,11 +14,10 @@ import java.util.List;
  * @author wn
  * @version $v: 1.0.0, $time:2019-04-04 15:18:00 Exp $
  */
-public interface PhLaborPrizeRepository extends JpaRepository<PhLaborPrize,Long>,JpaSpecificationExecutor<PhLaborPrize> {
+public interface PhLaborPrizeRepository extends JpaRepository<PhLaborPrize, Long>, JpaSpecificationExecutor<PhLaborPrize> {
+    @Query(nativeQuery = true, value = "select * from ph_labor_prize where type=?1 and `status`='0' ORDER BY RAND() limit 1")
+    PhLaborPrize lottery(String type);
 
-	
-	@Query(nativeQuery = true,value="select * from ph_labor_prize where type=?1 and `status`='0' ORDER BY RAND() limit 1")
-	PhLaborPrize lottery(String type);
-	
-	PhLaborPrize findByTypeAndStatusAndRemark(String type,String status,String remark);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    PhLaborPrize findByTypeAndStatusAndRemark(String type, String status, String remark);
 }
