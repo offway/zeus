9de01ea92f0b6e75e6d33eb55cfebd33801849c1
XiaZhengXin <xzx@xiazhengxin.name>
2019-12-26 16:20:03 +0800

    测试 GrowingIO 统计点 调用SDK API发送事件。。。。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/test/java/cn/offway/zeus/ZeusApplicationTests.java
+++ src/test/java/cn/offway/zeus/ZeusApplicationTests.java
@@ -1,5 +1,7 @@
 package cn.offway.zeus;
 
+import io.growing.sdk.java.GrowingAPI;
+import io.growing.sdk.java.dto.GIOEventMessage;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +20,21 @@ public class ZeusApplicationTests {
     public void testDate() {
         DateTime now = new DateTime();
         boolean a = now.getMonthOfYear() != 10 || now.getDayOfMonth() > 7;
+    }
+
+    @Test
+    public void testStat() {
+        //事件行为消息体
+        GIOEventMessage eventMessage = new GIOEventMessage.Builder()
+                .eventTime(System.currentTimeMillis())            // 事件时间，默认为系统时间（选填）
+                .eventKey("elevenProductClick")                           // 事件标识 (必填)
+                .loginUserId("uuuuserId")                   // 登录用户ID (必填)
+                .addEventVariable("place", "ni cai")          // 事件级变量 (选填)
+                .addEventVariable("channel", "1")      // 事件级变量 (选填)
+                .addEventVariable("productId", 666)            // 事件级变量 (选填)
+                .build();
+        //上传事件行为消息到服务器
+        GrowingAPI.send(eventMessage);
     }
 
 }
