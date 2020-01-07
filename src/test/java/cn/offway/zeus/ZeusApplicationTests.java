a36143acfd6cf9362470258ceac0038d7e6c8464
XiaZhengXin <xzx@xiazhengxin.name>
2020-01-07 16:01:42 +0800

    单元测试 JSON 字符串换行错误代码的复现、调试、修复。。。。。
    Signed-off-by: XiaZhengXin <xzx@xiazhengxin.name>

--- src/test/java/cn/offway/zeus/ZeusApplicationTests.java
+++ src/test/java/cn/offway/zeus/ZeusApplicationTests.java
@@ -1,16 +1,45 @@
 package cn.offway.zeus;
 
+import cn.offway.zeus.domain.PhConfig;
+import cn.offway.zeus.domain.PhGoods;
+import cn.offway.zeus.service.PhBannerService;
+import cn.offway.zeus.service.PhConfigService;
+import cn.offway.zeus.service.PhGoodsService;
+import cn.offway.zeus.utils.JsonResultHelper;
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.growing.sdk.java.GrowingAPI;
 import io.growing.sdk.java.dto.GIOEventMessage;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Profile("prd")
 public class ZeusApplicationTests {
+
+    @Autowired
+    private PhBannerService phBannerService;
+
+    @Autowired
+    private PhConfigService phConfigService;
+
+    @Autowired
+    private PhGoodsService phGoodsService;
+
+    @Autowired
+    private JsonResultHelper jsonResultHelper;
 
     @Test
     public void contextLoads() {
@@ -35,6 +64,37 @@ public class ZeusApplicationTests {
                 .build();
         //上传事件行为消息到服务器
         GrowingAPI.send(eventMessage);
+    }
+
+    @Test
+    public void testJson() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("banners", phBannerService.banners("4"));
+        List<PhConfig> configs = phConfigService.findByNameIn("INDEX_IMAGES_MINI", "INDEX_BRAND_GOODS_MINI", "INDEX_CATEGORY_MINI");
+        for (PhConfig phConfig : configs) {
+            String name = phConfig.getName().toLowerCase();
+            String content = phConfig.getContent();
+            if ("index_brand_goods_mini".equals(name)) {
+                List<Map> brands = JSON.parseArray(content, Map.class);
+                for (Map<String, Object> brand : brands) {
+                    Long brandId = Long.parseLong(brand.get("id").toString());
+                    List<Object> l = new ArrayList<>();
+                    for (PhGoods tmp : phGoodsService.findBrandRecommend(brandId)) {
+                        ObjectMapper mapper = new ObjectMapper();
+                        Map<String, Object> m = mapper.convertValue(tmp, Map.class);
+                        for (String k : m.keySet()) {
+                            m.put(k, StringEscapeUtils.unescapeJava(StringEscapeUtils.escapeJava(String.valueOf(m.get(k))).replaceAll("\\\\u2028", "")));
+                        }
+                        l.add(m);
+                    }
+                    brand.put("goods", l);
+                }
+                map.put(name, brands);
+            } else {
+                map.put(name, JSON.parse(content));
+            }
+        }
+        jsonResultHelper.buildSuccessJsonResult(map);
     }
 
 }
